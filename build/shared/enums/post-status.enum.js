e60f6066eb47c87f72f1f16912cf791b22cf05a3
bolhy91 <bolhy91@gmail.com>
2023-01-31 16:04:23 -0500

    delete build

--- build/shared/enums/post-status.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PostStatus = void 0;
-var PostStatus;
-(function (PostStatus) {
-    PostStatus["DRAFTED"] = "DRAFTED";
-    PostStatus["DELETED"] = "DELETED";
-    PostStatus["PUBLISHED"] = "PUBLISHED";
-})(PostStatus = exports.PostStatus || (exports.PostStatus = {}));
